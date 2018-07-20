a80941ea282640763aaa58ac7506fb5f38fe6298
Chang <15335713+iamajar@users.noreply.github.com>
2018-07-20 17:57:44 +0800

    拦截器相关类和页面

--- src/main/java/com/eyeieye/melody/demo/web/action/interceptor/MethodInterceptor.java
+++ src/main/java/com/eyeieye/melody/demo/web/action/interceptor/MethodInterceptor.java
@@ -1,75 +1,86 @@
 package com.eyeieye.melody.demo.web.action.interceptor;
 
+import com.eyeieye.melos.servlet.HandlerInterceptor;
 import com.eyeieye.melos.util.ArrayUtil;
-import org.aopalliance.intercept.MethodInvocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import java.lang.reflect.Method;
 import java.util.Map;
 
 @Component
-public class MethodInterceptor implements org.aopalliance.intercept.MethodInterceptor {
+public class MethodInterceptor extends HandlerInterceptorAdapter implements HandlerInterceptor {
     private static final Logger logger = LoggerFactory.getLogger(MethodInterceptor.class);
 
     public void preInvoke(Method handlerMethod, Object handler, ServletWebRequest webRequest) {
-        StringBuffer sf = new StringBuffer();
-
-        sf.append("\n")
-                .append("        *-*-*-*-*-*-*-*-*-*-*-*[方法级别拦截器]*-*-*-*-*-*-*-*-*-*-*-*")
-                .append("\n")
-                .append("        *-*-*-*-*-*-*-*-*-*-*-*-[方法执行开始]-*-*-*-*-*-*-*-*-*-*-*-*")
-                .append("\n")
-                .append("        *                                                           *")
-                .append("\n")
-                .append("        *                                                           *")
-                .append("\n")
-                .append("        *                       方法名                              *")
-                .append("\n")
-                .append(handlerMethod.getName())
-                .append("\n")
-                .append("        *                       方法参数类型                         *")
-                .append("\n")
-                .append(ArrayUtil.toString(handlerMethod.getParameterTypes()))
-                .append("\n")
-                .append("        *                     方法返回值类型                         *")
-                .append("\n")
-                .append(handlerMethod.getReturnType().getName())
-                .append("\n")
-                .append("        *                        所在类名                            *")
-                .append("\n")
-                .append(handler.getClass().toString())
-                .append("\n");
-        logger.debug(sf.toString());
+        preAction(handler);
 
     }
 
     public void postInvoke(Method handlerMethod, Object handler, ServletWebRequest webRequest, ModelAndView mav) {
-        StringBuffer sf = new StringBuffer();
-
-        sf.append("\n").append("        *                     返回页面的参数                         *").append("\n");
-
-        if (mav == null || mav.getModelMap() == null) {
-            sf.append("null").append("\n");
-        } else {
-            for (Map.Entry entry : mav.getModelMap().entrySet()) {
-                sf.append(entry.getKey() + " : " + entry.getValue()).append("\n");
-            }
-        }
-
-        sf.append("        *                                                           *").append("\n")
-                .append("        *                                                           *").append("\n")
-                .append("        *                                                           *").append("\n")
-                .append("        *                                                           *").append("\n")
-                .append("        *-*-*-*-*-*-*-*-*-*-*-*-[方法执行结束]-*-*-*-*-*-*-*-*-*-*-*-*").append("\n");
-        logger.debug(sf.toString());
+        postAction(handler,mav);
     }
 
-    @Override
-    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
-        return null;
+    public void preAction(Object handler) {
+        if(handler instanceof HandlerMethod){
+            Method method = ((HandlerMethod)handler).getMethod();
+            StringBuffer sf = new StringBuffer();
+
+            sf.append("\n")
+                    .append("        *-*-*-*-*-*-*-*-*-*-*-*[方法级别拦截器]*-*-*-*-*-*-*-*-*-*-*-*")
+                    .append("\n")
+                    .append("        *-*-*-*-*-*-*-*-*-*-*-*-[方法执行开始]-*-*-*-*-*-*-*-*-*-*-*-*")
+                    .append("\n")
+                    .append("        *                                                           *")
+                    .append("\n")
+                    .append("        *                                                           *")
+                    .append("\n")
+                    .append("        *                       方法名                              *")
+                    .append("\n")
+                    .append(method.getName())
+                    .append("\n")
+                    .append("        *                       方法参数类型                         *")
+                    .append("\n")
+                    .append(ArrayUtil.toString(method.getParameterTypes()))
+                    .append("\n")
+                    .append("        *                     方法返回值类型                         *")
+                    .append("\n")
+                    .append(method.getReturnType().getName())
+                    .append("\n")
+                    .append("        *                        所在类名                            *")
+                    .append("\n")
+                    .append(handler.getClass().toString())
+                    .append("\n");
+            logger.debug(sf.toString());
+        }
+    }
+
+    public void postAction(Object handler, ModelAndView mav){
+        if(handler instanceof HandlerMethod){
+            Method method = ((HandlerMethod)handler).getMethod();
+            StringBuffer sf = new StringBuffer();
+
+            sf.append("\n").append("        *                     返回页面的参数                         *").append("\n");
+
+            if (mav == null || mav.getModelMap() == null) {
+                sf.append("null").append("\n");
+            } else {
+                for (Map.Entry entry : mav.getModelMap().entrySet()) {
+                    sf.append(entry.getKey() + " : " + entry.getValue()).append("\n");
+                }
+            }
+
+            sf.append("        *                                                           *").append("\n")
+                    .append("        *                                                           *").append("\n")
+                    .append("        *                                                           *").append("\n")
+                    .append("        *                                                           *").append("\n")
+                    .append("        *-*-*-*-*-*-*-*-*-*-*-*-[方法执行结束]-*-*-*-*-*-*-*-*-*-*-*-*").append("\n");
+            logger.debug(sf.toString());
+        }
     }
 }
--- src/main/java/com/eyeieye/melody/demo/web/action/login/ExtendedUserInterceptor.java
+++ src/main/java/com/eyeieye/melody/demo/web/action/login/ExtendedUserInterceptor.java
@@ -1,57 +1,62 @@
-//package com.eyeieye.melody.demo.web.action.login;
-//
-//import com.eyeieye.melody.demo.cache.CacheManager;
-//import com.eyeieye.melody.web.adapter.AnnotationMethodHandlerInterceptorAdapter;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.core.annotation.AnnotationUtils;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.context.request.RequestAttributes;
-//import org.springframework.web.context.request.ServletWebRequest;
-//
-//import java.lang.reflect.Method;
-//
-//@Component
-//public class ExtendedUserInterceptor extends AnnotationMethodHandlerInterceptorAdapter {
-//    @Autowired
-//    private CacheManager<ExtendedUserCacheEntry> cacheManager;
-//
-//    /**
-//     * 缓存名称
-//     */
-//    private  String cacheName = ExtendedUserCacheEntry.class.getName();
-//
-//    @Override
-//    public void preInvoke(Method handlerMethod, Object handler,
-//                          ServletWebRequest webRequest){
-//        Extended extended = AnnotationUtils.getAnnotation(
-//                handlerMethod, Extended.class);
-//        if(extended == null){
-//            extended = handler.getClass().getAnnotation(Extended.class);
-//        }
-//
-//        if(extended == null){
-//            return;
-//        }
-//
-//        User user = (User)webRequest.getAttribute(User.NAME, RequestAttributes.SCOPE_SESSION);
-//        if(user == null) return;
-//
-//        ExtendedUserCacheEntry extendedUserCacheEntry = cacheManager.getData(cacheName,user.getUuid());
-//
-//        if(extendedUserCacheEntry == null) {
-//            extendedUserCacheEntry = new ExtendedUserCacheEntry();
-//        }
-//        ExtendedUser exUser = extendedUserCacheEntry.getExtendedUser();
-//        if(exUser == null){
-//            exUser = new ExtendedUser();
-//        }
-//        exUser.setUser(user);
-//
-//        webRequest.setAttribute(ExtendedUser.NAME, exUser,RequestAttributes.SCOPE_REQUEST);
-//
-//
-//    }
-//
-//
-//
-//}
+package com.eyeieye.melody.demo.web.action.login;
+
+import com.eyeieye.melody.demo.cache.CacheManager;
+import com.eyeieye.melos.servlet.HandlerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Method;
+
+@Component
+public class ExtendedUserInterceptor extends HandlerInterceptorAdapter implements HandlerInterceptor {
+    @Autowired
+    private CacheManager<ExtendedUserCacheEntry> cacheManager;
+
+    /**
+     * 缓存名称
+     */
+    private  String cacheName = ExtendedUserCacheEntry.class.getName();
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if(handler instanceof HandlerMethod){
+            Method handlerMethod = ((HandlerMethod)handler).getMethod();
+            Extended extended = AnnotationUtils.getAnnotation(
+                    handlerMethod, Extended.class);
+            if(extended == null){
+                extended = handler.getClass().getAnnotation(Extended.class);
+            }
+
+            if(extended == null){
+                return false;
+            }
+
+            User user = (User)request.getSession().getAttribute(User.NAME);
+
+            if(user == null) return false;
+
+            ExtendedUserCacheEntry extendedUserCacheEntry = cacheManager.getData(cacheName,user.getUuid());
+
+            if(extendedUserCacheEntry == null) {
+                extendedUserCacheEntry = new ExtendedUserCacheEntry();
+            }
+            ExtendedUser exUser = extendedUserCacheEntry.getExtendedUser();
+            if(exUser == null){
+                exUser = new ExtendedUser();
+            }
+            exUser.setUser(user);
+
+            request.getSession().setAttribute(ExtendedUser.NAME, exUser);
+
+            return true;
+        }
+        return false;
+    }
+}
